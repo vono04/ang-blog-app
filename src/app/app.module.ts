609c7b155bdc57990ee5eed23b1da7004d137d73
vono04 <60944909+vono04@users.noreply.github.com>
2023-10-10 17:38:21 +0200

    Design Post Card I

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { SubscriptionFormComponent } from './subscription-form/subscription-form
 import { CommentFormComponent } from './comments/comment-form/comment-form.component';
 import { CommentListComponent } from './comments/comment-list/comment-list.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { AppRoutingModule } from './app-routing.module';
     ContactUsComponent,
     SubscriptionFormComponent,
     CommentFormComponent,
-    CommentListComponent
+    CommentListComponent,
+    PostCardComponent
   ],
   imports: [
     BrowserModule,
